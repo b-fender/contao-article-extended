905cd0ad1fed9030463d7c2d674dc0044effcb16
b-fender <b-fender@users.noreply.github.com>
2026-09-09 12:53:53 +0000

    Fix: getPath() überschrieben, damit Contao contao/dca, templates und config im Paket-Root findet (nicht in src/)

--- src/ArticleExtendedBundle.php
+++ src/ArticleExtendedBundle.php
@@ -8,4 +8,15 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ArticleExtendedBundle extends Bundle
 {
+    /**
+     * Ohne diese Überschreibung liefert Bundle::getPath() das Verzeichnis
+     * dieser Klasse (also "src/") zurück. Contao sucht Ressourcen wie
+     * contao/dca/*.php, templates/* und config/* aber im PAKET-
+     * Wurzelverzeichnis (eine Ebene über src/) – deshalb wurden bislang
+     * weder die DCA-Erweiterungen noch das Backend-Modul gefunden.
+     */
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
